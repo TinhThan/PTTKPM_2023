50bc2ace9ffc222179f16944ee344b13eacc8417
tinh.than <22424018@student.hcmus.edu.vn>
2024-01-04 21:51:49 +0700

    Create resouce

---
+++ PTTKPM/XML/ThoiKhoaBieu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTTKPM.XML
+{
+    class ThoiKhoaBieu
+    {
+        List<Thu> dsThu = new List<Thu>();
+
+        internal List<Thu> DsThu
+        {
+            get { return dsThu; }
+            set { dsThu = value; }
+        }
+    }
+}
